797bb3b44066a6727c907b3699887ade66846c7a
g4tsn <64555267+g4tsn@users.noreply.github.com>
2022-08-13 11:50:32 +0100

    Update telegrambot.sh

--- install/telegrambot.sh
+++ install/telegrambot.sh
@@ -5,7 +5,7 @@ PATH_RUN_SCRIPT=$1
 	pip install telepot
 	sleep 2
 
-	echo 'Crezione dei servizi.....'
+	echo 'Create  service.....'
 
 	cp -R ${PATH_RUN_SCRIPT}/telegrambot.py /home/pi/
 	cp -R ${PATH_RUN_SCRIPT}/service/telegrambot.service /lib/systemd/system/
