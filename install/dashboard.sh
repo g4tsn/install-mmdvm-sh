3987eda068aaa7503937f5aae697a2603c72d43f
g4tsn <64555267+g4tsn@users.noreply.github.com>
2022-08-31 00:18:21 +0100

    Update dashboard.sh

--- install/dashboard.sh
+++ install/dashboard.sh
@@ -4,6 +4,7 @@
 
 	git clone https://github.com/dg9vh/MMDVMHost-Dashboard.git /var/www/html/MMDVMHost-Dashboard
 
+        c383e537e55a22d237cfcd82641d82f35d195a32
 	groupadd www-data
 	usermod -G www-data -a pi
 	chown -R www-data:www-data /var/www/html
