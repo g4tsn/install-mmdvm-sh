91493630ec3b943bab64bdac830e48d4aea619bc
g4tsn <64555267+g4tsn@users.noreply.github.com>
2022-08-31 01:15:22 +0100

    Update dmrgateway.sh

--- install/dmrgateway.sh
+++ install/dmrgateway.sh
@@ -17,7 +17,7 @@ sleep 2
 	else
 		make -j$N_CPU all
 	fi
-
+        cp /opt/install-mmdvm-sh/install/DMRGateway.ini /home/pi/MMDVM/DMRGateway/DMRGatway.ini 
 	cp -R /home/pi/MMDVM/DMRGateway/DMRGateway ${PATH_EXEC}
 	mkdir -p ${CONFIG_PATH_DMRGATEWAY}
 	mkdir -p ${LOG_PATH_DMRGATEWAY}
@@ -28,16 +28,17 @@ sleep 2
 	# sed -i 's,FileLevel=1,FileLevel=0,g' ${CONFIG_PATH_DMRGATEWAY}MMDVM.ini
 	# sed -i 's,# Port=/dev/ttyACM0,Port=/dev/ttyAMA0,g' ${CONFIG_PATH_DMRGATEWAY}MMDVM.ini
 	# sed -i 's,Port=\\.\COM3,# Port=\\.\COM3,g' ${CONFIG_PATH_DMRGATEWAY}MMDVM.ini
-        cp -R /opt/install-mmdvm-sh/install/DMRGateway.ini /home/pi/MMDVM/DMRGateway/DMRGatway.ini
+        
 	
 	nano DMRGateway.ini
 
 	cp -R /home/pi/MMDVM/DMRGateway/DMRGateway.ini ${CONFIG_PATH_DMRGATEWAY}
 	cp -R /home/pi/MMDVM/DMRGateway/XLXHosts.txt ${LOG_PATH_DMRGATEWAY}
 	cp -R /home/pi/MMDVM/DMRGateway/Audio ${CONFIG_PATH_DMRGATEWAY}
-	cp -R ${PATH_RUN_SCRIPT}/service/dmrgateway.service /lib/systemd/system/
-	cp -R ${PATH_RUN_SCRIPT}/service/dmrgateway.timer /lib/systemd/system/
+	cp -R /opt/install-mmdvm-sh/service/dmrgateway.service /lib/systemd/system/
+	cp -R /opt/install-mmdvm-sh/service/dmrgateway.timer /lib/systemd/system/
 	chmod 755 /lib/systemd/system/dmrgateway.service	
 	chmod 755 /lib/systemd/system/dmrgateway.timer
-	
+	systemctl enable dmrgateway.service
+	systemctl enable dmrgateway.timer
 exit 0
